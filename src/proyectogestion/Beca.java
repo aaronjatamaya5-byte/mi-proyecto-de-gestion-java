8147ebc3a5bd0e9de9cd889e11851bde6d5a8b69
aaron <aaron@DESKTOP-HEQ3VLJ>
2026-08-15 17:23:45 -0400

    Configuracion inicial del proyecto y clases base

---
+++ src/proyectogestion/Beca.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectogestion;
+
+/**
+ *
+ * @author aaron
+ */
+public class Beca {
+    private int codigo;
+    private String nombre;
+    private int puntajeMinimo;
+    private int cupos;
+       
+}
---
+++ src/proyectogestion/Estudiante.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectogestion;
+
+/**
+ *
+ * @author aaron
+ */
+public class Estudiante {
+    private String nombre;
+    private int edad;
+    private String rut;
+    private String sexo;
+    private String direccionHogar;
+    private String direccionEstadia;
+    private double porcentajeRegistro;
+    private String estado;
+    private int nivelDeportividad;
+    private int ingresosFamiliares;
+}
